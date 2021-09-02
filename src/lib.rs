1a1009977fdb730e3379ac77a034c4254a217e6a
caverym <caverym@disroot.org>
2021-09-02 15:12:46 -0400

    Separate Proton's Path from Proton
    Signed-off-by: caverym <caverym@disroot.org>

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,16 @@ View [bin/proton-call.rs](bin/proton-call.rs) for more information about the `pr
 
 use std::ffi::OsString;
 use std::io::{Error, ErrorKind, Result};
+use std::fmt::Formatter;
 
 /// This is the latest version of Proton at the time of modification.
 pub static PROTON_LATEST: &str = "6.3";
 
+#[derive(Debug)]
 /// Main struct for PC, manages and contains all needed information to properly execute Proton.
 pub struct Proton {
     steam: String,
-    proton: String,
+    proton: ProtonPath,
     executable: String,
     passed_args: Vec<OsString>,
     data: String,
@@ -33,7 +35,7 @@ impl Proton {
 
         let steam: String = config.get_steam();
         let data: String = config.get_data();
-        let proton: String = args.get_proton(&common);
+        let proton: ProtonPath = args.get_proton(&common);
         let executable: String = args.get_executable();
         let passed_args: Vec<OsString> = args.get_extra_args();
         let log: bool = args.get_log();
@@ -56,7 +58,7 @@ impl Proton {
     pub fn check(&self) -> Result<()> {
         use std::path::Path;
 
-        if !Path::new(&self.proton).exists() {
+        if !Path::new(&self.proton.path()).exists() {
             return Err(Error::new(ErrorKind::NotFound, format!("{} not found!", self.proton)));
         }
 
@@ -76,11 +78,14 @@ impl Proton {
     pub fn run(self) -> Result<()> {
         use std::process::{Child, Command};
 
+        dbg!(&self);
+        dbg!(self.proton.name());
+
         let log = if self.log { "1" } else { "0" };
 
         println!("\n________Proton________");
 
-        let mut child: Child = Command::new(self.proton)
+        let mut child: Child = Command::new(self.proton.path())
             .arg("run")
             .arg(self.executable)
             .args(self.passed_args)
@@ -100,6 +105,60 @@ impl Proton {
     }
 }
 
+#[derive(Debug, Clone)]
+pub enum ProtonPath {
+    Custom {
+        path: String,
+    },
+
+    Steam {
+        version: String,
+        name: String,
+        path: String,
+    }
+}
+
+impl ProtonPath {
+    pub fn path(&self) -> String {
+        match self {
+            Self::Custom { path } => path.clone(),
+            Self::Steam { path, .. } => path.clone(),
+        }
+    }
+
+    pub fn version(&self) -> Option<String> {
+        match self {
+            Self::Custom { .. } => None,
+            Self::Steam { version, .. } => Some(version.clone()),
+        }
+    }
+
+    pub fn name(&self) -> String {
+        match self {
+            Self::Custom { path } => {
+                let ret: Vec<&str> = path.split('/').collect();
+                for s in ret {
+                    if s == "proton" {
+                        continue;
+                    } else if s.starts_with("Proton") {
+                        return s.to_string()
+                    }
+                }
+                "custom".to_string()
+            },
+            Self::Steam { name, .. } => name.clone()
+        }
+    }
+}
+
+impl std::fmt::Display for ProtonPath {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}",
+            self.name()
+        )
+    }
+}
+
 /// Trait used in the Proton struct to get information from the Config.
 pub trait ProtonConfig {
     /// Return `steam` directory where Steam is installed.
@@ -115,7 +174,7 @@ pub trait ProtonConfig {
 /// Trait used in the Proton struct to get argument information from Command line.
 pub trait ProtonArgs {
     /// Return full path to the requested proton executable.
-    fn get_proton(&self, common: &str) -> String;
+    fn get_proton(&self, common: &str) -> ProtonPath;
 
     /// Return path to the Windows executable to run.
     fn get_executable(&self) -> String;
